0c2d07780ab7fc4287796a00dfc7a33e5fddfb53
BuiXuanHuynh1995 <huynhxuanbui@gmail.com>
2021-03-04 20:09:17 +0700

    gia tri bieu thuc

--- src/GiaTriBT.java
+++ src/GiaTriBT.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class GiaTriBT {
+    public static void main(String[] args) {
+        int rs =0;
+        int n;
+        Scanner scanner =new Scanner(System.in);
+        do {
+            System.out.println("Nhap n:");
+            n=scanner.nextInt();
+        }while (n<0);
+
+        for (int i=1;i<=n;i++){
+            if (i%2==0){
+                rs-=i;
+            }else {
+                rs+=i;
+            }
+        }
+        System.out.println(rs);
+    }
 }
