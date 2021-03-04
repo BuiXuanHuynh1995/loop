920ed7ac7f78532a5be323da127c4d007f839b40
BuiXuanHuynh1995 <huynhxuanbui@gmail.com>
2021-03-04 20:14:57 +0700

    break

---
+++ src/Break.java
@@ -0,0 +1,17 @@
+public class Break {
+    public static void main(String[] args) {
+//        for (int i=1;i<=10;i++){
+//            System.out.println("Lần thứ " + i);
+//            System.out.println("Chào bạn");
+//            if (i==3){
+//                break;
+//            }
+//        }
+        for (int i=10;i>=0;i--){
+            System.out.println(i);
+            if (i<=5){
+                break;
+            }
+        }
+    }
+}
