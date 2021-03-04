0eb655956ac5ae3b67a0717d47343ba6a7c0e5e3
BuiXuanHuynh1995 <huynhxuanbui@gmail.com>
2021-03-04 17:14:14 +0700

    in ra các số chẵn nhỏ hơn hoặc bằng 20.

---
+++ src/SoChan.java
@@ -0,0 +1,15 @@
+public class SoChan {
+    public static void main(String[] args) {
+        int number=0;
+//        while (number<=20){
+//            if(number%2==0){
+//                System.out.println(number);
+//            }
+//            number++;
+//        }
+        while (number<=20){
+            System.out.println(number);
+            number+=2;
+        }
+    }
+}
